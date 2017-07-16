2acf8c3154697832a97ccb2ec6b2dcaf3b6df8f4
Eliott Ham <eliotthham@gmail.com>
2017-07-16 14:45:55 -0700

    Build Airport and Flight models with associations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Airport.delete_all
+Flight.delete_all
+
+# Create airports
+codes = [ "SFO", "JFK", "MEM", "MIA" ]
+names = [ "(SFO) - San Francisco Intl., San Francisco, CA",
+					"(JFK) - John F. Kennedy Intl., New York, NY",
+					"(MEM) - Memphis Intl., Memphis, TN",
+					"(MIA) - Miami Intl., Miami, FL" ]
+for i in 0..4
+	Airport.create(code: codes[i], name: names[i])
+end
+
+# Create flights
+pairs = Airport.all.to_a.permutation(2).to_a 
+
+pairs.each do |pair|
+	10.times { Flight.create(date: Time.new.to_date + rand(10).day,
+													 time: Time.new + rand(24).hour,
+													 duration: rand(3..6),
+													 from_airport: pair[0],
+													 to_airport: pair[1]) }
+end
